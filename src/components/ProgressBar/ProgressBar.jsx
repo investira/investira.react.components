35efcc5c0220c8fb6a263d38c43d75f160b5074f
willlucas1512 <willlucas1512@gmail.com>
2022-04-12 17:27:31 -0300

    Update ProgressBar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,42 +1,44 @@
-import React from 'react';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import Style from './ProgressBar.module.scss';
+import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import Style from "./ProgressBar.module.scss";
 
 function ProgressBar(props) {
-    const xClass = classNames(Style.progressBarFill, props.className, {
-        [Style.animate]: props.animate === 'indeterminate'
-    });
+  const xSpanClass = classNames(Style.progressBarFill, {
+    [Style.animate]: props.animate === "indeterminate",
+  });
 
-    return (
-        <>
-            <div className={Style.progressBar}>
-                <span
-                    style={{
-                        width: `${String(props.value)}%`,
-                        backgroundColor: `var(--color-${props.color})`
-                    }}
-                    className={xClass}></span>
-                <span
-                    style={{
-                        backgroundColor: `var(--color-${props.color})`
-                    }}
-                    className={xClass}></span>
-            </div>
-        </>
-    );
+  const xRootClass = classNames(Style.progressBar, props.className);
+
+  return (
+    <div className={xRootClass}>
+      <span
+        style={{
+          width: `${String(props.value)}%`,
+          backgroundColor: `var(--color-${props.color})`,
+        }}
+        className={xSpanClass}
+      ></span>
+      <span
+        style={{
+          backgroundColor: `var(--color-${props.color})`,
+        }}
+        className={xSpanClass}
+      ></span>
+    </div>
+  );
 }
 
 ProgressBar.propTypes = {
-    color: PropTypes.string,
-    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    animate: PropTypes.oneOf(['indeterminate', 'progress'])
+  color: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  animate: PropTypes.oneOf(["indeterminate", "progress"]),
 };
 
 ProgressBar.defaultProps = {
-    color: 'primary',
-    value: 100,
-    animate: 'progress'
+  color: "primary",
+  value: 100,
+  animate: "progress",
 };
 
 export default ProgressBar;
