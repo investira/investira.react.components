faddbcfe5f6f56b32daf0a7ccc1e465fa5f5569f
Magno Valdetaro <mvaldetaro@gmail.com>
2022-04-18 10:08:13 -0300

    Implementa sortByAttr

--- src/components/utils/helpers.js
+++ src/components/utils/helpers.js
@@ -21,3 +21,22 @@ export function isTouchDevice() {
   }
   return false;
 }
+
+/**
+ * Retorna um array de objetos ordenado pelo campo definido no parametro pAttr
+ * @param {Array | Object} pData array ou objeto de objetos
+ * @param {string} pAttr campo a ser ordenado
+ */
+export function sortByAttr(pData, pAttr) {
+  const xDataOrdered = Object.values(pData).sort((a, b) => {
+    if (a[pAttr] < b[pAttr]) {
+      return -1;
+    }
+    if (a[pAttr] > b[pAttr]) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return xDataOrdered;
+}
