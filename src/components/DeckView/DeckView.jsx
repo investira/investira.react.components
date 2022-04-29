bbc6963c64be485259c08358007c505d04386dd6
Magno Valdetaro <mvaldetaro@gmail.com>
2022-04-29 11:13:59 -0300

    Remove espaços

--- src/components/DeckView/DeckView.jsx
+++ src/components/DeckView/DeckView.jsx
@@ -3,7 +3,6 @@ import DeckContext from "../DeckContext";
 
 const DeckView = memo((props) => {
   const deckContext = useContext(DeckContext);
-
   /* Implementar aqui, uma condição para verificar se a View está ativa. Analisar os possíveis efeitos colaterais. */
 
   return React.cloneElement(props.children, {
