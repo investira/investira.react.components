3fc7c626dc424c8684dd55db61b5c60a2019d1d3
willlucas1512 <willlucas1512@gmail.com>
2022-08-30 17:09:45 -0300

    add componentes

--- src/components/template/Error/Error.jsx
+++ src/components/template/Error/Error.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Box } from "../../";
 import PropTypes from "prop-types";
-import Style from "./Error.module.scss";
 function Error(props) {
   return (
     <Box
@@ -30,28 +29,119 @@ function Error(props) {
           width={props.width ? `${props.width}px` : null}
           height={props.height ? `${props.height}px` : null}
           viewBox="0 0 512 512"
-          //   sx={[props.startAnimation && {}]}
-          className={props.startAnimation ? Style.start : undefined}
         >
-          <path
-            sx={{
-              fill: "none",
-              stroke: "#ed4420",
-              strokeWidth: 40,
-              strokeMiterlimit: 10,
-            }}
+          <Box
+            component="path"
+            sx={[
+              {
+                fill: "none",
+                stroke: "#ed4420",
+                strokeWidth: 40,
+                strokeMiterlimit: 10,
+              },
+              props.startAnimation && {
+                strokeDasharray: "1473 1475",
+                strokeDashoffset: "1474",
+                animation:
+                  "draw 690ms ease-in 100ms forwards, fade 690ms ease-in 100ms forwards",
+                "@keyframes draw": {
+                  "100%": {
+                    strokeDashoffset: 0,
+                  },
+                },
+                "@keyframes fade": {
+                  "0%": {
+                    strokeOpacity: 0,
+                  },
+
+                  "93.54838709677419%": {
+                    strokeOpacity: 0.93,
+                  },
+
+                  "100%": {
+                    strokeOpacity: 1,
+                  },
+                },
+              },
+            ]}
             d="M22.49999999999997,255.2A234.4,234.4 0,1,1 491.29999999999995,255.2A234.4,234.4 0,1,1 22.49999999999997,255.2"
-          ></path>
-          <path
-            className={Style.line}
-            //className="st1 cPKHNHna_1"
+          ></Box>
+          <Box
+            component="path"
+            sx={[
+              {
+                fill: "none",
+                stroke: "#ed4420",
+                strokeWidth: 40,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeMiterlimit: 10,
+              },
+              props.startAnimation && {
+                strokeDasharray: "201 203",
+                strokeDashoffset: "202",
+                animation:
+                  "draw 490ms ease-in 300ms forwards, fadeLine 490ms ease-in 300ms forwards",
+                "@keyframes draw": {
+                  "100%": {
+                    strokeDashoffset: 0,
+                  },
+                },
+                "@keyframes fadeLine": {
+                  "0%": {
+                    strokeOpacity: 0,
+                  },
+
+                  "43.54838709677419%": {
+                    strokeOpacity: 0.93,
+                  },
+
+                  "50%": {
+                    strokeOpacity: 1,
+                  },
+                },
+              },
+            ]}
             d="M185.5,183.3L327.2,325.1"
-          ></path>
-          <path
-            className={Style.line2}
-            //className="st1 cPKHNHna_2"
+          ></Box>
+          <Box
+            component="path"
+            sx={[
+              {
+                fill: "none",
+                stroke: "#ed4420",
+                strokeWidth: 40,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeMiterlimit: 10,
+              },
+              props.startAnimation && {
+                strokeDasharray: "201 203",
+                strokeDashoffset: "202",
+                animation:
+                  "draw 490ms ease-in 300ms forwards, fadeLine 490ms ease-in 300ms forwards",
+                "@keyframes draw": {
+                  "100%": {
+                    strokeDashoffset: 0,
+                  },
+                },
+                "@keyframes fadeLine": {
+                  "0%": {
+                    strokeOpacity: 0,
+                  },
+
+                  "43.54838709677419%": {
+                    strokeOpacity: 0.93,
+                  },
+
+                  "50%": {
+                    strokeOpacity: 1,
+                  },
+                },
+              },
+            ]}
             d="M185.5,325.1L327.2,183.3"
-          ></path>
+          ></Box>
         </svg>
       </Box>
     </Box>
