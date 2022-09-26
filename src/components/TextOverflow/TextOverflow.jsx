87a0d1785711ddb44208bd779f45f75d1154a1b2
Magno Valdetaro <mvaldetaro@gmail.com>
2022-09-26 13:19:51 -0300

    Correção no estilo

--- src/components/TextOverflow/TextOverflow.jsx
+++ src/components/TextOverflow/TextOverflow.jsx
@@ -7,26 +7,36 @@ const Fluid = styled("div")(() => {
   return {
     flex: "1 1 100%",
     minWidth: 0,
-    whiteSpace: "nowrap",
   };
 });
 
 const TextOverflow = memo((props) => {
   return (
-    <Fluid>
-      <Typography
-        {...props}
-        component="p"
-        sx={{
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-          whiteSpace: "nowrap",
-          ...props.sx,
-        }}
-      >
-        {props.children}
-      </Typography>
-    </Fluid>
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+      }}
+    >
+      <Fluid>
+        <Typography
+          {...props}
+          component="p"
+          sx={{
+            textOverflow: "ellipsis",
+            overflow: "hidden",
+            display: "-webkit-box",
+            lineClamp: "1",
+            boxOrient: "vertical",
+            "-webkit-line-clamp": "1",
+            "-webkit-box-orient": "vertical",
+            ...props.sx,
+          }}
+        >
+          {props.children}
+        </Typography>
+      </Fluid>
+    </Box>
   );
 });
 
