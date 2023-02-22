b0fec41965141950deeaeb5755ee9abdf5637ddb
Magno Valdetaro <mvaldetaro@gmail.com>
2023-02-22 16:46:20 -0300

    Update Backdrop.jsx

--- src/components/wrappers/Backdrop/Backdrop.jsx
+++ src/components/wrappers/Backdrop/Backdrop.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { default as WBackdrop } from "@mui/material/Backdrop";
+import { default as MuiBackdrop } from "@mui/material/Backdrop";
 
-function Button(props) {
-  return <WBackdrop {...props} />;
+function Backdrop(props) {
+  return <MuiBackdrop {...props} />;
 }
 
-export default Button;
+export default Backdrop;
