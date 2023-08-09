69d2bb15fe98109a37652fef17baaf80510300b4
Magno Valdetaro <mvaldetaro@gmail.com>
2023-08-09 18:19:13 -0300

    Add CSSBaseline e Tabcontainer

--- src/components/wrappers/index.js
+++ src/components/wrappers/index.js
@@ -75,6 +75,7 @@ export { default as Switch } from "./Switch";
 export { default as Table } from "./Table";
 export { default as TableBody } from "./TableBody";
 export { default as TableCell } from "./TableCell";
+export { default as TableContainer } from "./TableContainer";
 export { default as TableFooter } from "./TableFooter";
 export { default as TableHead } from "./TableHead";
 export { default as TablePagination } from "./TablePagination";
