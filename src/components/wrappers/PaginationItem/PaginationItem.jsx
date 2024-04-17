28432133cb8c5960d845a41ab829544817b65554
Magno Valdetaro <mvaldetaro@gmail.com>
2024-04-17 11:36:50 -0300

    Add PaginationItem

---
+++ src/components/wrappers/PaginationItem/PaginationItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { default as MuiPaginationItem } from "@mui/material/PaginationItem";
+
+function PaginationItem(props) {
+  return <MuiPaginationItem {...props} />;
+}
+
+export default PaginationItem;
