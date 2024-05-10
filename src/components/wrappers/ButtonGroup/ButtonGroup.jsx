fbff6dddb3961aca7995a09d4b4e7c52581d2aa1
Magno Valdetaro <mvaldetaro@gmail.com>
2024-05-10 17:09:25 -0300

    Add ButtonGroup e FormGroup

---
+++ src/components/wrappers/ButtonGroup/ButtonGroup.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { default as MuiButtonGroup } from "@mui/material/ButtonGroup";
+
+function ButtonGroup(props) {
+  return <MuiButtonGroup {...props} />;
+}
+
+export default ButtonGroup;
---
+++ src/components/wrappers/FormGroup/FormGroup.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { default as MuiFormGroup } from "@mui/material/FormGroup";
+
+function FormGroup(props) {
+  return <MuiFormGroup {...props} />;
+}
+
+export default FormGroup;
