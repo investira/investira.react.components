beef9e011ca00c115386d7ab9bca2d1c3d938ff9
Magno Valdetaro <mvaldetaro@gmail.com>
2026-04-30 16:04:07 -0300

    Update Badge.jsx

--- src/components/wrappers/Badge/Badge.jsx
+++ src/components/wrappers/Badge/Badge.jsx
@@ -1,72 +1,72 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { default as WBadge } from "@mui/material/Badge";
+import { default as MuiBadge } from "@mui/material/Badge";
 
-const StyledBadge = styled(WBadge, {
-  shouldForwardProp: (prop) =>
-    !["specialAnchor", "sizeCustom"].includes(prop.toString()),
-})(({ theme, specialAnchor, sizeCustom }) => {
-  const base = {};
+// const StyledBadge = styled(WBadge, {
+//   shouldForwardProp: (prop) =>
+//     !["specialAnchor", "sizeCustom"].includes(prop.toString()),
+// })(({ theme, specialAnchor, sizeCustom }) => {
+//   const base = {};
 
-  if (specialAnchor) {
-    base["& .MuiBadge-badge"] = {
-      ...(base["& .MuiBadge-badge"] || {}),
-      right: "40%",
-    };
-  }
+//   if (specialAnchor) {
+//     base["& .MuiBadge-badge"] = {
+//       ...(base["& .MuiBadge-badge"] || {}),
+//       right: "40%",
+//     };
+//   }
 
-  if (sizeCustom === "XL") {
-    base["& .MuiBadge-badge"] = {
-      ...(base["& .MuiBadge-badge"] || {}),
-      width: theme.spacing(5),
-      height: theme.spacing(5),
-      borderRadius: theme.spacing(4),
-    };
-    base["& .MuiBadge-badge svg"] = {
-      width: "24px",
-      height: "24px",
-    };
-  }
+//   if (sizeCustom === "XL") {
+//     base["& .MuiBadge-badge"] = {
+//       ...(base["& .MuiBadge-badge"] || {}),
+//       width: theme.spacing(5),
+//       height: theme.spacing(5),
+//       borderRadius: theme.spacing(4),
+//     };
+//     base["& .MuiBadge-badge svg"] = {
+//       width: "24px",
+//       height: "24px",
+//     };
+//   }
 
-  if (sizeCustom === "large") {
-    base["& .MuiBadge-badge"] = {
-      ...(base["& .MuiBadge-badge"] || {}),
-      width: theme.spacing(4),
-      height: theme.spacing(4),
-      borderRadius: theme.spacing(3),
-    };
-    base["& .MuiBadge-badge svg"] = {
-      width: "21px",
-      height: "21px",
-    };
-  }
+//   if (sizeCustom === "large") {
+//     base["& .MuiBadge-badge"] = {
+//       ...(base["& .MuiBadge-badge"] || {}),
+//       width: theme.spacing(4),
+//       height: theme.spacing(4),
+//       borderRadius: theme.spacing(3),
+//     };
+//     base["& .MuiBadge-badge svg"] = {
+//       width: "21px",
+//       height: "21px",
+//     };
+//   }
 
-  if (sizeCustom === "small") {
-    base["& .MuiBadge-badge"] = {
-      ...(base["& .MuiBadge-badge"] || {}),
-      width: theme.spacing(2),
-      height: theme.spacing(2),
-      borderRadius: theme.spacing(2),
-    };
-    base["& .MuiBadge-badge svg"] = {
-      width: "10px",
-      height: "10px",
-    };
-  }
+//   if (sizeCustom === "small") {
+//     base["& .MuiBadge-badge"] = {
+//       ...(base["& .MuiBadge-badge"] || {}),
+//       width: theme.spacing(2),
+//       height: theme.spacing(2),
+//       borderRadius: theme.spacing(2),
+//     };
+//     base["& .MuiBadge-badge svg"] = {
+//       width: "10px",
+//       height: "10px",
+//     };
+//   }
 
-  return base;
-});
+//   return base;
+// });
 
 function Badge(props) {
   const { anchorOrigin, size, ...propsProps } = props;
   const specialAnchor = anchorOrigin?.special;
 
   return (
-    <StyledBadge
+    <MuiBadge
       {...propsProps}
       anchorOrigin={anchorOrigin}
-      specialAnchor={specialAnchor}
-      sizeCustom={size}
+      //specialAnchor={specialAnchor}
+      //sizeCustom={size}
     />
   );
 }
